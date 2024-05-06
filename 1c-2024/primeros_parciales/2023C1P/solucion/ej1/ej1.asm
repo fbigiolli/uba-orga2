023753bb602e35dacb5f06a6cb1ec21eef5a2240
fbigiolli <felipebigiolli@gmail.com>
2024-05-06 09:47:33 -0300

    2023C1P ej1

--- 1c-2024/primeros_parciales/2023C1P/solucion/ej1/ej1.asm
+++ 1c-2024/primeros_parciales/2023C1P/solucion/ej1/ej1.asm
@@ -7,7 +7,7 @@
 global templosClasicos
 global cuantosTemplosClasicos
 
-
+extern malloc
 
 ;########### SECCION DE TEXTO (PROGRAMA)
 section .text
@@ -20,69 +20,57 @@ templosClasicos:
     mov rbp, rsp
     push r12
     push r13 ; stack alineado
-    push r14
-    push r15 ; stack alineado
-    push rbx
-    sub rsp, 8
 
-    xor r15, r15 ; r15 -> offset array input
-    xor rbx, rbx ; rbx -> offset array resultado
+    mov r12, rdi ; r12 -> puntero array templos
+    mov r13, rsi ; r13 -> size array
+    
+    call cuantosTemplosClasicos ; rax -> cant templos clasicos
 
-    mov r12, rdi ; r12 -> preserva puntero array templos
-    mov r13, rsi ; r13 -> preserva size array
+    xor r9, r9 
+    mov r9, SIZE_TEMPLO 
 
-    xor rdx, rdx
-    mov rdx, 8
+    mul r9 ; rax -> cant templos clasicos * size templo
+    mov rdi, rax
+    call malloc ; rax -> puntero a array res
 
-    call cuantosTemplosClasicos ; rax -> cant de templos clasicos
-    mul rdx ; rax -> cant de templos clasicos * size puntero
-    mov rdi, rax ; rdi -> cant de templos clasicos * size puntero
-    call malloc ; rax -> puntero a array a devolver
-    mov r14, rax ; r14 -> puntero a array a devolver 
+    xor rdx, rdx ; rdx -> offset array resultado
+    xor rcx, rcx ; rcx -> offset array input
 
     .ciclo:
-        cmp r13, 0 ; termino de recorrer el array
+        cmp r13, 0
         je .end
 
-        mov al, [r12 + r15 + OFFSET_LADO_CORTO] ; al -> lado corto
-        mov cl, [r12 + r15 + OFFSET_LADO_LARGO] ; cl -> lado largo
+        xor r8, r8 ; r8 -> lados cortos en byte
+        xor r9, r9 ; r9 -> lados largos en byte
 
-        mov rdi, 2 ; rdi para multiplicar x2
-        mul dil ; al -> 2N
-        inc al ; al -> 2N + 1
+        mov r8b, [r12 + rcx + OFFSET_LADO_CORTO] ; r8b -> lado corto
+        mov r9b, [r12 + rcx + OFFSET_LADO_LARGO] ; r9b -> lado largo
 
-        cmp al, cl
+        shl r8, 1 ; r8b -> 2N
+        inc r8 ; r8b -> 2N + 1
+
+        cmp r8, r9
         jne .siguiente ; si no son iguales, no es templo clasico
 
-        ; son iguales, pido memoria para un templo y lo copio
-        mov rdi, SIZE_TEMPLO 
-        call malloc ; rax -> puntero a struct templo
+        ; son iguales, lo copio a res
+        mov r8b, [r12 + rcx + OFFSET_LADO_CORTO] ; r8b -> lado corto 
+        mov [rax + rdx + OFFSET_LADO_CORTO], r8b ; mando a memoria
 
-        mov cl, [r12 + r15 + OFFSET_LADO_LARGO] ; cl -> lado largo
-        mov [rax + OFFSET_LADO_LARGO], cl
+        mov r8b, [r12 + rcx + OFFSET_LADO_LARGO] ; r8b -> lado largo 
+        mov [rax + rdx + OFFSET_LADO_LARGO], r8b ; mando a memoria
 
-        mov rcx, [r12 + r15 + OFFSET_NOMBRE] ; rcx -> nombre
-        mov [rax + OFFSET_NOMBRE], rcx
-        
-        mov cl, [r12 + r15 + OFFSET_LADO_CORTO] ; cl -> lado corto
-        mov [rax + OFFSET_LADO_CORTO], cl
+        mov r8b, [r12 + rcx + OFFSET_NOMBRE] ; r8 -> nombre 
+        mov [rax + rdx + OFFSET_NOMBRE], r8b ; mando a memoria
 
-        mov [r14 + rbx], rax ; manda a memoria el puntero reservado
-        add rbx, 8 ; rbx -> offset a proxima posicion del array
+        add rdx, SIZE_TEMPLO ; muevo puntero res
 
         .siguiente:
-            add r15, 8  ; r15 -> proximo puntero a templo
             dec r13
-            jmp .ciclo ; loop
+            add rcx, SIZE_TEMPLO ; muevo puntero array original y decremento size array
+            jmp .ciclo
 
-
-    ; epilogo
     .end:
-        mov rax, r14 ; rax -> puntero a array res
-        add rsp, 8
-        pop rbx
-        pop r15
-        pop r14
+        ; epilogo
         pop r13
         pop r12
         pop rbp
@@ -96,35 +84,33 @@ cuantosTemplosClasicos:
     push rbp
     mov rbp, rsp
 
-    xor r8, r8 ; r8 -> offset array
-    xor r9, r9 ; r9 -> acumulador
-    xor rbx, rbx ; rbx -> tiene el 2 para multiplicar
-    xor rax, rax ; rax -> lados cortos en byte   
-    xor rcx, rcx ; rcx -> lados largos en byte
-    mov rbx, 2
-
+    xor rax, rax ; rax -> acumulador
+    xor rcx, rcx ; rcx -> offset array 
+    
     .ciclo: 
         cmp rsi, 0 ; fin del array
         je .end
 
-        mov al, [rdi + r8 + OFFSET_LADO_CORTO] ; al -> lado corto
-        mov cl, [rdi + r8 + OFFSET_LADO_LARGO] ; cl -> lado largo
+        xor r8, r8 ; r8 -> lados cortos en byte
+        xor r9, r9 ; r9 -> lados largos en byte
 
-        mul bl ; al -> 2N
-        inc al ; al -> 2N + 1
+        mov r8b, [rdi + rcx + OFFSET_LADO_CORTO] ; r8b -> lado corto
+        mov r9b, [rdi + rcx + OFFSET_LADO_LARGO] ; r9b -> lado largo
 
-        cmp al, cl
+        shl r8, 1 ; r8b -> 2N
+        inc r8 ; r8b -> 2N + 1
+
+        cmp r8, r9
         jne .siguiente ; si no son iguales, no es templo clasico
-        inc r9 ; son iguales, aumento contador
+        inc rax ; son iguales, aumento contador
 
         .siguiente:
+            add rcx, SIZE_TEMPLO  ; rcx -> proximo puntero a templo
             dec rsi ; rsi -> size - 1
-            add r8, 8  ; r8 -> proximo puntero a templo
             jmp .ciclo
 
     ; epilogo
     .end:
-        mov rax, r9 ; rax -> total templos clasicos 
         pop rbp
         ret
 
